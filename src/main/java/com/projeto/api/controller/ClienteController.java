46c6adfa34a04fcb1b666123295d9f1b55a98ee0
Leonard107 <leonardrichard107@gmail.com>
2022-03-14 19:17:23 -0300

    usando Spring JPA

--- src/main/java/com/projeto/api/controller/ClienteController.java
+++ src/main/java/com/projeto/api/controller/ClienteController.java
@@ -1,33 +1,24 @@
 package com.projeto.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.domain.model.Cliente;
+import com.projeto.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> Listar() {
-		Cliente cliente1 = new Cliente();
-		
-		cliente1.setId(1L);
-		cliente1.setNome("Leonard Richard");
-		cliente1.setTelefone("99247-1050");
-		cliente1.setEmail("leonardrichard107@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		
-		cliente2.setId(2L);
-		cliente2.setNome("Carlinhos");
-		cliente2.setTelefone("98247-2011");
-		cliente2.setEmail("carlinhos@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 
 }
