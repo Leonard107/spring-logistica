c346dac1af9d5fb6372414f63cfd9f9c3d775b42
Leonard107 <leonardrichard107@gmail.com>
2022-04-14 15:55:07 -0300

    Propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/projeto/domain/model/Cozinha.java
+++ src/main/java/com/projeto/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.projeto.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +18,8 @@ public class Cozinha {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@Column(nullable = false)
 	private String nome;
 
 }
--- src/main/java/com/projeto/domain/model/Restaurante.java
+++ src/main/java/com/projeto/domain/model/Restaurante.java
@@ -21,13 +21,14 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false)
 	private String nome;
 	
-	@Column(name = "taxa_frete")
+	@Column(name = "taxa_frete", nullable = false)
 	private Double taxaFrete;
 	
 	@ManyToOne
-	@JoinColumn(name = "cozinha_id")
+	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 
 	
