4da7bd39e8a5d9cd09fc4105137d36c83444b04d
Leonard107 <leonardrichard107@gmail.com>
2022-04-14 09:52:31 -0300

    A anotacao @JoinColumn

--- src/main/java/com/projeto/domain/model/Restaurante.java
+++ src/main/java/com/projeto/domain/model/Restaurante.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -26,6 +27,7 @@ public class Restaurante {
 	private Double taxaFrete;
 	
 	@ManyToOne
+	@JoinColumn(name = "cozinha_id")
 	private Cozinha cozinha;
 
 	
