60c5d76b07032f9a08a867da898e7584064c5186
Leonard107 <leonardrichard107@gmail.com>
2022-05-22 00:23:15 -0300

    Usando queries JPQL customizadas com @Query

--- src/main/java/com/projeto/api/controller/TesteController.java
+++ src/main/java/com/projeto/api/controller/TesteController.java
@@ -43,7 +43,7 @@ public class TesteController {
 	
 	@GetMapping("/restaurantes/por-nome-e-idcozinha")
 	public List<Restaurante> restaurantePorNomeEiDCozinha(String nome, Long cozinhaId) {
-		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+		return restauranteRepository.consultarPorNome(nome, cozinhaId);
 	}
 
 }
