602d529df66fa5c3672bb029cf1d960cbacf8350
Leonard107 <leonardrichard107@gmail.com>
2022-05-22 22:45:48 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/projeto/domain/repository/RestauranteRepository.java
+++ src/main/java/com/projeto/domain/repository/RestauranteRepository.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +14,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	
 	List<Restaurante> findBytaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
 	
