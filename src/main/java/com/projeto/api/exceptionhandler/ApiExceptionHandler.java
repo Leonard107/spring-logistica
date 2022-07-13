f5d3c0d05f5f8d533b0e1ae9afd77704cb7d87d5
Leonard107 <leonardrichard107@gmail.com>
2022-07-13 18:10:43 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/com/projeto/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/projeto/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,16 +4,16 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.projeto.domain.exception.EntidadeEmUsoException;
 import com.projeto.domain.exception.EntidadeNaoEncontradaException;
 import com.projeto.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e){
@@ -21,7 +21,7 @@ public class ApiExceptionHandler {
 		Problema problema = Problema.builder()
 				.dataHora(LocalDateTime.now())
 				.mensagem(e.getMessage()).build();
-		
+
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(problema);
 	}
@@ -35,18 +35,6 @@ public class ApiExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.body(problema);
-	}
-	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-		
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem("O tipo de mídia não é aceito.").build();
-		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-				.body(problema);
-		
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
