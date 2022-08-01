923bd333ee9ad79e3caef29fbaa30f3049701c0d
Leonard107 <leonardrichard107@gmail.com>
2022-07-31 23:43:20 -0300

    Convertendo grupos de constraints para validação em cascata com @ConvertGroup

--- src/main/java/com/projeto/Groups.java
+++ src/main/java/com/projeto/Groups.java
@@ -2,6 +2,6 @@ package com.projeto;
 
 public interface Groups {
 	
-	public interface CadastroRestaurante {}
+	public interface CozinhaId {}
 
 }
--- src/main/java/com/projeto/api/controller/RestauranteController.java
+++ src/main/java/com/projeto/api/controller/RestauranteController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.projeto.Groups;
 import com.projeto.domain.exception.CozinhaNaoEncontradaException;
 import com.projeto.domain.exception.NegocioException;
 import com.projeto.domain.model.Restaurante;
@@ -58,7 +56,7 @@ public class RestauranteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Restaurante adicionar(@RequestBody @Validated(Groups.CadastroRestaurante.class) Restaurante restaurante) {
+	public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {
 		try {
 			return cadastroRestauranteService.salvar(restaurante);
 		} catch (CozinhaNaoEncontradaException e) {
