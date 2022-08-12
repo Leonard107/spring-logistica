e9963393a0147c32ff441766493dd617a266efba
Leonard107 <leonardrichard107@gmail.com>
2022-08-12 16:46:14 -0300

    Configurando um banco de testes e usando @TestPropertySource

--- src/test/java/com/projeto/CadastroCozinhaIT.java
+++ src/test/java/com/projeto/CadastroCozinhaIT.java
@@ -1,12 +1,15 @@
 package com.projeto;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import io.restassured.RestAssured;
@@ -14,16 +17,22 @@ import io.restassured.http.ContentType;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private Flyway flyway;
 	
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 		
 	}
 		
@@ -53,4 +62,19 @@ class CadastroCozinhaIT {
 			.body("", Matchers.hasSize(4))
 			.body("nome", Matchers.hasItems("Indiana","Tailandesa"));
 	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCOzinha() {
+		
+		RestAssured.given()
+			.body("{ \"nome\": \"Chinesa\"  }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
+	
 }
