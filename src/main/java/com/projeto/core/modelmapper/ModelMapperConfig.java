12c3ba939f6c4682cb88be32b641df175130b2bb
Leonard107 <leonardrichard107@gmail.com>
2022-09-21 20:09:19 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

--- src/main/java/com/projeto/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/projeto/core/modelmapper/ModelMapperConfig.java
@@ -4,19 +4,27 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.projeto.domain.model.Endereco;
 import com.projeto.domain.model.Restaurante;
+import com.projeto.domain.model.DTO.EnderecoDTO;
 import com.projeto.domain.model.DTO.RestauranteDTO;
 
 @Configuration
 public class ModelMapperConfig {
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
-		
-		modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class);
-		
-		
+
+		//modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class);
+		//		.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
+
+		enderecoToEnderecoModelTypeMap.<String>addMapping(
+				origemEndereco -> origemEndereco.getCidade().getEstado().getNome(),
+				(destinoEnderecoDTO, valor) -> destinoEnderecoDTO.getCidade().setEstado(valor));
+
 		return modelMapper;
 	}
 
---
+++ src/main/java/com/projeto/domain/model/DTO/CidadeResumoDTO.java
@@ -0,0 +1,14 @@
+package com.projeto.domain.model.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CidadeResumoDTO {
+	
+	private Long id;
+    private String nome;
+    private String estado;
+
+}
